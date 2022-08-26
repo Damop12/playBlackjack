4b7ff0666f0b024c3b047cd62131779f2519f984
David Ozorio <damop12@gmail.com>
2022-08-26 12:36:30 -0400

    init

--- 14-Repaso/app.js
+++ 14-Repaso/app.js
@@ -16,11 +16,21 @@
 
 
 const getUsuarios = () => {
-    setTimeout  (() => {
+    setTimeout(() => {
         const userIDS = [100, 103, 107];
         console.log(userIDS);
+        setTimeout((id) => {
+            const usuario = {
+                email: 'mateo@mail.com',
+                nombre: 'Mateo',
 
-    }, 1500);     //5 parte 3 4:40min
+            }
+            console.log(`${id}--${usuario.email}--${usuario.nombre}`);
+
+        }, 1500,userIDS[2]);
+    }, 1500);
+    //5 parte 3 4:40min
 
 }
+
 getUsuarios();
